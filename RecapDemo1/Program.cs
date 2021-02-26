a241665a0b8425a505e11221e32690afd7a165d9
Doga <dogaguven2000@hotmail.com>
2021-02-26 14:53:36 +0300

    Proje dosyası ekle.

---
+++ RecapDemo1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapDemo1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Button[,] buttons = new Button[8,8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    if ((i+j)%2==0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+
+                    this.Controls.Add(buttons[i, j]);
+                }
+                top += 50;
+                left = 0;
+            }
+            
+
+        }
+    }
+}
